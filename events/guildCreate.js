3cfd2829b847fc7485642e6c396897ad63315976
DA JUUKES <dajuukes@gmail.com>
2018-03-03 20:57:22 -0800

    improved join mgg

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -4,14 +4,14 @@ exports.run = (bot, guild) => {
 
   let emb = new Discord.RichEmbed();
   let description = "" +
-    "My prefix here is `%`.\n" +
+    "My default prefix here is `%`.\n" +
     "You can see a list of commands by typing `%help`.\n" +
     "You can change my prefix with `%setprefix <newprefix>`.\n" +
-    "If you need help, feel free to join our support server at  https://github.com/dajuukes/DisCrypto. "
-  
+    "If you need help, feel free to join our support server at https://discord.gg/Xg5V8mn. "
+
   emb.setTitle("Thank you for adding me!")
       .setColor(`#00FF00`)
       .setDescription(description);
-  
+
   guild.defaultChannel.send(emb);
 };
