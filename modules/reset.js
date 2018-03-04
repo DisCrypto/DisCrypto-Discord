372b6e8e3fbe5a81eb46ae9f0058f505b051cd15
DA JUUKES <dajuukes@gmail.com>
2018-03-04 12:23:01 -0800

    fixed reset and git pull

--- modules/reset.js
+++ modules/reset.js
@@ -22,6 +22,17 @@ module.exports = {
                 bot.shard.broadcastEval('process.exit(0)');
             }, 1000);
             return null;
+        } else if (msg.args[0] == "git") {
+            msg.channel.send(':wave: All shards of ' + bot.user.username + ' are restarting to git pull...');
+
+            setTimeout(() => {
+                bot.shard.broadcastEval(`git pull`).then(()=> {
+                    bot.shard.broadcastEval('process.exit(0)').then(() => {
+                        message.channel.send(`Resetted and pulled!`);
+                    }).catch(err => {msg.reply(`Error`); console.error(err);});
+                }).catch(err => {msg.reply(`Error`); console.error(err);});
+            }, 1000);
+            return null;
         }
     },
 };
