a17ccb88701569d50e1128491175d3a5198c712a
DA JUUKES <dajuukes@gmail.com>
2018-03-04 08:08:11 -0800

    webhook pls

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -7,7 +7,7 @@ exports.run = (bot, guild) => {
     My default prefix here is \`%\`.\n
 You can see a list of commands by typing \`%help\`.\n
 You can change my prefix with \`%setprefix <newprefix>\`.\n
-If you need help, feel free to join our support server at https://discord.gg/Xg5V8mn.`;
+If you need help, feel free to join our support server at https://discord.gg/Xg5V8mn !`;
 
     emb.setTitle("Thank you for adding me!")
         .setColor(`#00FF00`)
