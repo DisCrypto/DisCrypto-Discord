5c1bf7e8e615c692b824a625f16135bf818c856a
samuspro <samusnane@gmail.com>
2018-03-03 23:21:39 -0500

    add welcome message when bot is added

---
+++ events/guildCreate.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+
+exports.run = (bot, guild) => {
+
+  let emb = new Discord.RichEmbed();
+  let description = "" +
+    "My prefix here is `%`.\n" +
+    "You can see a list of commands by typing `%help`.\n" +
+    "You can change my prefix with `%setprefix <newprefix>`.\n" +
+    "If you need help, feel free to join our support server at  https://github.com/dajuukes/DisCrypto. "
+  
+  emb.setTitle("Thank you for adding me!")
+      .setColor(`#00FF00`)
+      .setDescription(description);
+  
+  guild.defaultChannel.send(emb);
+};
