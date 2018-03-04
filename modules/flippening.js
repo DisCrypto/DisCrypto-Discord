636f766be95c745897526acc6d1567616015d761
DA JUUKES <dajuukes@gmail.com>
2018-03-04 14:43:46 -0800

    push test

--- modules/flippening.js
+++ modules/flippening.js
@@ -20,7 +20,7 @@ module.exports = {
             let percent = Math.trunc((res.body[1].market_cap_usd / res.body[0].market_cap_usd) * 100);
             console.log((res.body[0].market_cap_usd / res.body[0].market_cap_usd));
             let emb = new Discord.RichEmbed()
-                .setTitle(`ETH/BTC Flippening`)
+                .setTitle(`ETH/BTC Fldippening`)
                 .setDescription(`The Flippening is where Ethereum overtakes Bitcoin as #1 in Market Cap. \n \nCurrently Ethereum is **${percent}%** along the way to overtaking BTC.\n\nEthereum Market Cap: **$${addCommas(res.body[1].market_cap_usd)}**\n\nBitcoin Market Cap: **$${addCommas(res.body[0].market_cap_usd)}**`)
                 .setColor(`ORANGE`)
                 .attachFile(`./data/icons/flippening.jpg`)
