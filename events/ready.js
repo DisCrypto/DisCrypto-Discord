362952d31457d1f494a5d94e72971efeed3efe17
DA JUUKES <dajuukes@gmail.com>
2018-03-05 17:52:04 -0800

    fixed reset reload

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,8 @@ exports.run = bot => {
     bot.sendServerCount();
     bot.syncServers();
     bot.startGameCycle();
-    bot.awaitConsoleInput();
+    delete require.cache[require.resolve(`./reset.js`)]; //seriously idk why but it always needs to be reloaded
+    bot.commands.set('reset', require(`./reset.js`));
 
     bot.log(`${bot.user.username} is online and ready to serve in ${bot.channels.size} channels on ${bot.guilds.size} servers!`);
 
