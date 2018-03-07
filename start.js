2ec29b3a01eb57f21b7b37a58b339aba369a41ac
DA JUUKES <dajuukes@gmail.com>
2018-03-06 20:06:25 -0800

    delet

--- start.js
+++
@@ -1,8 +0,0 @@
-const Discord = require('discord.js');
-const config = require('./config.json');
-const Manager = new Discord.ShardingManager('./bot.js', { totalShards: config.shards, token: config.token });
-Manager.spawn();
-
-Manager.on('message', (shard, msg) => {
-    console.log('[' + shard.id + 1 + '] ' + msg);
-});
