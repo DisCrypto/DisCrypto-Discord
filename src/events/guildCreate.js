67ff9af19a8dd70198a921a7dc79171d802c5c82
DA JUUKES <dajuukes@gmail.com>
2018-03-12 17:49:33 -0700

    guildCreate updoot

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -7,7 +7,8 @@ exports.run = (bot, guild) => {
     My default prefix here is \`%\`.\n
 You can see a list of commands by typing \`%help\`.\n
 You can change my prefix with \`%setprefix <newprefix>\`.\n
-If you need help, feel free to join our support server at https://discord.gg/Xg5V8mn !`;
+If you need help, feel free to join our support server at https://discord.gg/Xg5V8mn \n
+If you like my features, I'd really appreciate a free and quick upvote on our Bot List over [here](https://discordbots.org/bot/411996950670344234)!`;
 
     emb.setTitle("Thank you for adding me!")
         .setColor(`#00FF00`)
